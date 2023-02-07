6836a78ace1781e587a5f856aab10a4362390af1
Sam Lerner <sam.a.lerner@gmail.com>
2023-02-07 09:28:42 -0500

    server.js refactored

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+// mongodb+srv://mongo:mongo@cluster0.gnx5xkw.mongodb.net/booksDB?retryWrites=true&w=majority
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/where-books-at', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
