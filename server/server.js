3db13e0b7478915606cf663d7d1fd07351eda57b
Sam Lerner <sam.a.lerner@gmail.com>
2023-02-08 09:45:33 -0500

    schemas underway

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
-const express = require('express');
 // Apollo components
 const { ApolloServer } = require('apollo-server-express');
+const {authMiddleware} = require('./utils/auth');
+
+// Importing schemas
+const {typeDefs, resolvers} = require('./schemas');
 
 
 const path = require('path');
@@ -13,25 +16,27 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
 
+const express = require('express');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
+  
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+  
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+  
 
   db.once('open', () => {
     app.listen(PORT, () => {
